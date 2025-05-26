8dd2933f7be05b61265d945d50087dab88262290
Karolina Bock <78617160+karolinabock@users.noreply.github.com>
2025-05-26 16:04:01 +0200

    Add files via upload

---
+++ CREATE_TABLE.sql
@@ -0,0 +1,12 @@
+CREATE TABLE employees(
+employee_id INT PRIMARY KEY,
+first_name TEXT,
+last_name TEXT,
+job_position TEXT,
+salary NUMERIC(8,2),
+start_date DATE,
+birth_date DATE,
+store_id INT,
+departament_id INT,
+manager_id INT
+)
